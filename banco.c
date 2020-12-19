53229d6570a64c5b69f285db4d08dcbaf520eaf3
Edgar A. Dikenge <dikengeofficial@gmail.com>
2020-12-19 14:32:12 +0100

    Alterando a opção levantamento 1º

--- banco.c
+++ banco.c
@@ -8,20 +8,18 @@ int main(){
 // variables
 	int PIN = 1234;
 	int codigo, operacao;
-	float money = 100000;
-	float saque;
 
 //Init a loop para que não ficarmos toda hora ter que reiniciar o terminal	
 	while ("true")
 	{
-		
-	printf("=================###=======#########==========####=====####==========\n");
-	printf("================#####======#########===========###====###============\n");
-	printf("===============#######========##===============###===###=============\n");
-	printf("===============##===##========##===============####=###==============\n");
-	printf("===============#######========##===============########==============\n");
-	printf("===============##===##========##===============####===###============\n");
-	printf("===============##===##========##===============####===###============\n");
+		system("clear");
+	printf("==================###======#########====####======####=================\n");
+	printf("=================#####=====#########====###=#====#=###=================\n");
+	printf("================#######=======##========###==#==#==###=================\n");
+	printf("================##===##=======##========###===##===###=================\n");
+	printf("================#######=======##========###========###=================\n");
+	printf("================##===##=======##========###========###=================\n");
+	printf("================##===##=======##========###========###=================\n");
 	printf("\n");
 	printf("\n");
 	printf("DIGITE O SEU PIN:");
@@ -38,11 +36,8 @@ int main(){
 		printf("=====================================================================\n");
 		printf("\n");
 		printf("\n");
-		printf(" => 1) LEVANTAMENTO   \t\t\t");
-		printf(" => 2) TRÂNSFERENCIA \n");
-		printf("\n");
-		printf(" => 3) PAGAMENTOS      \t\t\t");
-		printf(" => 4) CONSULTA\n\n => 5) Alterar PIN\n\n");
+		printf(" => 1) LEVANTAMENTO   \t\t\t => 2) TRÂNSFERENCIA \n\n");
+		printf(" => 3) PAGAMENTOS      \t\t\t => 4) CONSULTA\n\n => 5) Alterar PIN\n\n");
 		scanf("%i", &operacao);
 		system("clear");
 		
@@ -56,11 +51,34 @@ int main(){
 					printf("=====================================================================\n");
 					printf("\n");
 					printf("\n");
-					printf(" => 1) 2.000 KZS                        ");
+					printf(" => 1) 2.000 KZS     \t\t\t\t");
 					printf(" => 2) 5.000 KZS \n");
 					printf("\n");
-					printf(" => 3) 10.0000 KZS                      ");
+					printf(" => 3) 10.0000 KZS \t\t\t\t");
 					printf(" => 4) OUTRO MONTANTE \n");
+					scanf("%d",&operacao);
+					system("clear");
+
+					printf("\n\nPós levantamento Retirar Cartão Multicaixa ? \n");
+					printf(" 1) ==> Sim \t\t\t\t 2) ==> Não\n");
+					scanf("%d",&operacao);
+
+					if (operacao == 1)
+					{
+						system("clear");
+						printf("\n\n\t\t\t\tFaz de conta que teu Dinheiro Saíu $$$$$ :)\n");
+					}
+					else if(operacao == 2)
+					{
+						system("clear");
+						printf("\n\n\t\t\t\tFaz de conta que teu Dinheiro Saíu $$$$$ :)\n");
+						system("exit");
+					}
+					else{
+						printf("Operação não Encontrada !!\n");
+					}
+					
+
 			break;
 			case 3:
 					printf("=====================================================================\n");
@@ -68,8 +86,7 @@ int main(){
 					printf("=========================== Trânsferencia ===========================\n");
 					printf("=====================================================================\n");
 					printf("=====================================================================\n");
-					printf("\n");
-					printf("\n");
+					printf("\n\n");
 			break;
 			
 			// Consultas
@@ -79,8 +96,7 @@ int main(){
 					printf("=========================== CONSULTA ================================\n");
 					printf("=====================================================================\n");
 					printf("=====================================================================\n");
-					printf("\n");
-					printf("\n");
+					printf("\n\n");
 					printf(" => 1) CONSULTAR SALDO DA CONTA\n");
 					printf(" => 2) CONSULTAR IBAN	\n\n");
 					printf(" => 0) CANCELAR \t\t\t\t\t\t\t => 3) OUTRAS OPÇÕES\n");
@@ -103,7 +119,8 @@ int main(){
 						
 						
 						default :
-						printf("OPÇÃO NÃO ENCONTRADA :( test");
+						
+						printf("OPÇÃO NÃO ENCONTRADA :(");
 						system("exit");
 					
 					}
